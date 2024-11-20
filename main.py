9a3198ca29c3a72e75624a75e0b950ef5031f966
Lheendah <linchinemerem@gmail.com>
2024-11-20 10:48:36 +0100

    updated main.py
    Added a function to calculate the area of a circle

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-print("hello!")
-print("world!")
+# create an application to calculate the area of a circle
+
+PI = 3.142
+
+def area_of_a_circle(radius):
+    r = int(radius)
+    return PI * r * r
+
+area = area_of_a_circle(10)
+print(area)
